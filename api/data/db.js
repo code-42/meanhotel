e1b2d513adba9a246d520a637c695fb11f13ad4c
Edward Dupre <edwd42@gmail.com>
2017-09-26 18:22:11 -0400

    push working copy from Linux box then clone to Mac

--- api/data/db.js
+++ api/data/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var dburl = 'mongodb://localhost:27017/meanhotel';
-
+var retry = null;
 mongoose.connect(dburl);
 
 mongoose.connection.on('connected',function(){
@@ -15,26 +15,55 @@ mongoose.connection.on('error',function(err){
 	console.log('Mongoose connnection error: ' + err);
 });
 
-process.on('SIGINT', function(){
-	mongoose.connection.close(function(){
-		console.log('Mongoose disconnected through app termination (SIGINT)');
-		process.exit(0);
-	});
-});
+// process.on('SIGINT', function(){
+// 	mongoose.connection.close(function(){
+// 		console.log('Mongoose disconnected through app termination (SIGINT)');
+// 		process.exit(0);
+// 	});
+// });
+//
+// process.on('SIGTERM', function(){
+// 	mongoose.connection.close(function(){
+// 		console.log('Mongoose disconnected through app termination (SIGINT)');
+// 		process.exit(0);
+// 	});
+// });
+//
+// process.once('SIGUSR2', function(){
+// 	mongoose.connection.close(function(){
+// 		console.log('Mongoose disconnected through app termination (SIGUSR2)');
+// 		process.kill(process.pid, 'SIGUSR2');
+// 	});
+// });
 
-process.on('SIGTERM', function(){
-	mongoose.connection.close(function(){
-		console.log('Mongoose disconnected through app termination (SIGINT)');
-		process.exit(0);
-	});
-});
+// CAPTURE APP TERMINATION / RESTART EVENTS
+// To be called when process is restarted or terminated
+function gracefulShutdown(msg, callback) {
+  mongoose.connection.close(function() {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+}
 
-process.once('SIGUSR2', function(){
-	mongoose.connection.close(function(){
-		console.log('Mongoose disconnected through app termination (SIGUSR2)');
-		process.kill(process.pid, 'SIGUSR2');
-	});
+// For nodemon restarts
+process.once('SIGUSR2', function() {
+  gracefulShutdown('nodemon restart', function() {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+// For app termination
+process.on('SIGINT', function() {
+  gracefulShutdown('App termination (SIGINT)', function() {
+    process.exit(0);
+  });
+});
+// For Heroku app termination
+process.on('SIGTERM', function() {
+  gracefulShutdown('App termination (SIGTERM)', function() {
+    process.exit(0);
+  });
 });
 
 // bring in schemas and models
 require('./hotels.model.js');
+require('./users.model');
--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 var ctrlHotels = require('../controllers/hotels.controllers.js');
 var ctrlReviews = require('../controllers/reviews.controllers.js');
+var ctrlUsers = require('../controllers/users.controllers.js');
 
+// Hotel routes
 router
   .route('/hotels')
   .get(ctrlHotels.hotelsGetAll)
@@ -12,16 +14,27 @@ router
 router
   .route('/hotels/:hotelId')
   .get(ctrlHotels.hotelsGetOne)
-//  .put(ctrlHotels.hotelsUpdateOne);
+  // .put(ctrlHotels.hotelsUpdateOne);
 
+
+// review routes
 router
   .route('/hotels/:hotelId/reviews')
   .get(ctrlReviews.reviewsGetAll)
-  .post(ctrlReviews.reviewsAddOne);
+  .post(ctrlUsers.authenticate, ctrlReviews.reviewsAddOne);
 
 router
   .route('/hotels/:hotelId/reviews/:reviewId')
   .get(ctrlReviews.reviewsGetOne)
   .put(ctrlReviews.reviewsUpdateOne);
+
+// authentication
+router
+  .route('/users/register')
+  .post(ctrlUsers.register);
+
+  router
+    .route('/users/login')
+    .post(ctrlUsers.login);
 
 module.exports = router;
